7a7b3df4b490e41f53752a28116551f63f9ff28a
Caleb Broman <caleb.broman@gmail.com>
2026-06-15 16:37:34 -0500

    implemented a basic problem and a solver, added more files to ignore

--- R Code/scripts/basicProblemCreation.R
+++ R Code/scripts/basicProblemCreation.R
@@ -1,12 +1,21 @@
 #lower level function:
 
-lowerProblem <- function(x, ystar) {
-  
+lowerProblem <- function(z, x) {
+  2*x^2 + 2*x*z + (1/2)*z^2
+}
+
+#solution to lower level function given x:
+
+lowerProblemSolution <- function(x) {
+  map_dbl(x, \(x) optimize(lowerProblem, c(-10000,1+x), x=x)[[1]])
 }
 
 
 #upper level function:
 
-upperProblem <- function(x, z) {
-  
+upperProblem <- function(x) {
+  ystar <- lowerProblemSolution(x)
+  x^2 - ystar
 }
+
+print(optimize(upperProblem, c(-100, 100)))
